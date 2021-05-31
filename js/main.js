b2dd7b358c02f5397b0de020bc1b6327a3da40da
simon20136 <83860955+simon20136@users.noreply.github.com>
2021-06-01 08:44:35 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,14 +6,15 @@ const winner = Math.floor(Math.random() * num);
 for (let i=0; i<num; i++) {
     const div = document.createElement('div');
     div.classList.add('box');
+
     div.addEventListener('click', () => {
-        if(i === winner) {
+        if (i === winner) {
             div.classList.add('win');
             div.textContent = "あたり(^^)/";
         } else {
             div.classList.add('lose');
             div.textContent = "はずれ(T_T)";
         }
-    })
+    });
     document.body.appendChild(div);
 }
