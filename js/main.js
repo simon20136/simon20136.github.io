bfdf949db09f3572f3bcbafca773a10bb58ef83d
simon20136 <83860955+simon20136@users.noreply.github.com>
2021-09-14 09:46:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,58 @@
-'use strict';
+"use strict"
 
-const num = 9;
-const winner = Math.floor(Math.random() * num);
+{
+    game9boxProc();
+    btnClickProc();
 
-for (let i=0; i<num; i++) {
-    const div = document.createElement('div');
-    div.classList.add('box');
+    function game9boxProc() {
+        const hDiv = document.createElement('div');
+        hDiv.classList.add('h_box');
+        document.body.appendChild(hDiv);
+        hDiv.textContent = 'Check 9 box';
 
-    div.addEventListener('click', () => {
-        if (i === winner) {
-            div.classList.add('win');
-            div.textContent = "あたり(^^)/";
-        } else {
-            div.classList.add('lose');
-            div.textContent = "はずれ(T_T)";
+        const parentDiv = document.createElement('div');
+        parentDiv.classList.add('p_box');
+        document.body.appendChild(parentDiv);
+        
+        const num = 9;
+        const winner = Math.floor(Math.random() * num);
+    
+        let score = 100;
+        let flag = true;
+
+        for (let i=0; i<num; i++) {
+            const childDiv = document.createElement('div');
+            childDiv.classList.add('c_box');
+    
+            childDiv.addEventListener('click', () => {
+                if(i === winner) {
+                    childDiv.classList.add('win');
+                    childDiv.textContent = 'あたり (^^)/';
+                    flag = false;
+                    hDiv.textContent = '得点は' + String(score) + '点です';
+                } else {
+                    childDiv.classList.add('lose');
+                    childDiv.textContent = 'はずれ (T_T)';
+                    if(flag === true) {
+                        score -= 10;
+                    }
+                }
+            });
+            parentDiv.appendChild(childDiv);
         }
-    });
-    document.body.appendChild(div);
+    }
+    
+    function btnClickProc() {
+        const btnDiv = document.createElement('div');
+        btnDiv.classList.add('btn');
+        btnDiv.textContent = 'もう一回';
+        document.body.appendChild(btnDiv);
+        btnDiv.addEventListener('click', () => {
+            document.location.reload();
+        });
+    }
+
+    // C言語のように関数を定義したあとに置かなくてもよい
+    // game9boxProc();
+    // btnClickProc();
 }
